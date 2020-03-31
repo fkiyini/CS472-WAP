3d1ff10bce6910bd1eab547f1ca91af2631f9be6
fkiyini <kiyiniminafarouk@gmail.com>
2020-03-30 22:28:08 -0500

    Lab5 and Lab6 for css Layout

--- FormsAndLayout-Lab4/f2javaScript.js
+++ FormsAndLayout-Lab4/f2javaScript.js
@@ -1,3 +1,21 @@
 function displayAlert() {
     alert("Hello, world!");
+    alert(document.getElementById ("exampleInputDate"));
+}
+function displayForm1Input(){
+
+    Console.log()
+
+}
+function funcSubmit(event){
+    event.preventDefault();
+    const email = document.getElementById("eMail").value;
+    const website = document.getElementById("webSite").value;
+    const pWord = document.getElementById("passWord").value;
+    const chBox = document.getElementById("checkBOX").value;
+    console.log("email"+email);
+    console.log("website"+website);
+    console.log("pWord"+pWord);
+    console.log("chBox"+chBox);
+
 }
